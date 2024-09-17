801435a3dc32c6ba0e75263a11db89ba3c20970f
Marcelo Raineri <raineridev@gmail.com>
2024-09-17 21:49:59 +0100

    add: Header File

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+
+// Define
+
+
+// Functions: 
+
+/// Int Retun's
+
+int	ft_have_breakline(char *buffer);
+int	ft_strlen(char *str);
+
+/// Char Retun's
+
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
+
+
