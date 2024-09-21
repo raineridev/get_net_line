9c99d4ddce828e5775616709a89c873e7fb325b3
Marcelo Raineri <raineridev@gmail.com>
2024-09-22 00:01:33 +0100

    update:functions in utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mraineri <mraineri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 11:58:45 by mraineri          #+#    #+#             */
-/*   Updated: 2024/09/17 21:46:14 by mraineri         ###   ########.fr       */
+/*   Updated: 2024/09/22 00:00:43 by mraineri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,37 @@ char	*ft_strjoin(char *s1, char *s2)
 	int		i;
 	char	*str;
 
-	if(!s1 || !s2)
+	if(!s2)
 		return (NULL);
+	if(!s1)
+		return (s2);
 	str = malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
 	i = 0;
+	
 	while(*s1)
 		str[i++] = *s1++;
 	while(*s2)
 		str[i++] = *s2++;
+	if(s1)
+		free(s1);
 	return(str);
+}
+
+char *ft_strcut(char *line, char *buffer)
+{
+	int		i;
+	int		breakline;
+	char	*string;
+
+	i = 0;
+	breakline = (ft_have_breakline(buffer) + 1);
+	string = malloc(ft_have_breakline(line));
+	if(!string)
+		return (NULL);
+	while(buffer[i])
+		buffer[i++] = buffer[breakline++];
+    i = 0;
+	while(*line && *line != '\n')
+		string[i++] = *line++;
+	return (string);
 }
